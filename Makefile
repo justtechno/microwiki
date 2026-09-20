7dcb6f11f432a685803df57c0a2f01b622864d59
justtechno <daniz.ef2202@gmail.com>
2026-09-20 17:24:02 +0400

    fix makefiles, add platformdirs for better compatibility with different systems

--- Makefile
+++ Makefile
@@ -3,18 +3,20 @@ pyinstaller := uv run pyinstaller
 # variables for posix
 project := ./src/microwiki/main.py
 data := ./src/microwiki/data/:data
+src := ./src/microwiki/
 
 # variables for windows
 project_windows := .\src\microwiki\main.py
 data_windows := .\src\microwiki\main.py
+src_windows := .\src\microwiki\
 
 build:	# main build target for posix systems
 	uv sync
-	$(pyinstaller) --name="MicroWiki" --onefile --console --paths=. --noconfirm --collect-all textual --add-data "$(data)" $(project)
+	$(pyinstaller) --name="microwiki" --onefile --console --paths=$(src) --noconfirm --collect-all textual --add-data "$(data)" $(project)
 
 build_windows: # windows has other paths syntax so there are second build target for windows users
 	uv sync
-	$(pyinstaller) --name="MicroWiki" --onefile --console --paths=. --noconfirm --collect-all textual --add-data "$(data_windows)" $(project_windows)
+	$(pyinstaller) --name="microwiki" --onefile --console --paths=$(src_windows) --noconfirm --collect-all textual --add-data "$(data_windows)" $(project_windows)
 
 run:
 	uv sync && uv run $(project)
--- src/microwiki/utils/config_utils.py
+++ src/microwiki/utils/config_utils.py
@@ -4,10 +4,12 @@ import json
 import os
 from pathlib import Path
 
+from platformdirs import PlatformDirs
 
 def parse_config():
-    configdirpath = Path(f"{os.getenv('HOME')}/.config/microwiki/")
-    configfilepath = Path(f"{os.getenv('HOME')}/.config/microwiki/config.json")
+    pdirs = PlatformDirs("microwiki", "justtechno") 
+    configdirpath = pdirs.user_config_path
+    configfilepath = Path(f"{pdirs.user_config_dir}/config.json")
     if configdirpath.is_dir():
         with open(configfilepath, "r", encoding="utf-8") as file:
             config = json.load(file)
@@ -17,10 +19,8 @@ def parse_config():
         with open(configfilepath, "w", encoding="utf-8") as file:
             baseconfigdict = {
                 "theme": "nord",
-                "wikistorage": f"{os.getenv('HOME')}/.micronote/wikistorage/",
+                "wikistorage": f"{pdirs.user_data_dir}",
                 "allow_edit": True,
-                "editor": "nvim",
-                "lang": "eng",
             }
             baseconfigjson = json.dumps(baseconfigdict)
             file.write(baseconfigjson)
